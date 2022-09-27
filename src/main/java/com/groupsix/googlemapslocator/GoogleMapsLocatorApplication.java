fee7350403699b09ad0c47e7868af51974a7e8d5
Hayden50 <haydenrieder50@hotmail.com>
2022-09-27 15:35:10 -0400

    Initial commit

---
+++ src/main/java/com/groupsix/googlemapslocator/GoogleMapsLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.groupsix.googlemapslocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapsLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapsLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupsix/googlemapslocator/GoogleMapsLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupsix.googlemapslocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapsLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
