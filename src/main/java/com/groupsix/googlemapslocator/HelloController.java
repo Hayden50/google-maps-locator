d8f466038deedb232695c7ca9257dc43462d1683
Hayden50 <haydenrieder50@hotmail.com>
2022-09-28 15:06:45 -0400

    Clean up frontend / create proxy

--- src/main/java/com/groupsix/googlemapslocator/HelloController.java
+++ src/main/java/com/groupsix/googlemapslocator/HelloController.java
@@ -1,7 +1,5 @@
 package com.groupsix.googlemapslocator;
 
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+        return "Hello, the day at the server is now Wednesday\n";
     }
 }
