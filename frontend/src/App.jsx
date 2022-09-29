ddbe6caf712cb7a15863d73a0cf90fc5b56d3386
Hayden50 <haydenrieder50@hotmail.com>
2022-09-29 17:15:37 -0400

    Small cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
-  const[text, setText] = useState('');
-  
-  useEffect(() => {
-    fetch('/api/hello')
-        .then(response => response.text())
-        .then(message => setText(message))
-    },[]);
-  
+function App() {  
+
+  const [word, setWord] = useState('');
+
+  fetch('http://localhost:8080/api/data')
+    .then(response => response.toString())
+    .then(data => setWord(data));
+
   return (
     <div className="App">
-      <h1>{text}</h1>
+      <p>{word}</p>
     </div>
   )
 }
