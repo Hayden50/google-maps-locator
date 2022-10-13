612d7c6b867690daebea1917af945711976295e5
Hayden50 <haydenrieder50@hotmail.com>
2022-10-13 11:37:39 -0400

    Add table and styling to landing page

--- frontend/components/LocationSelector.jsx
+++ frontend/components/LocationSelector.jsx
@@ -1,11 +1,11 @@
 import {Formik, Field, Form} from 'formik';
 import axios from 'axios';
+import './LocationSelector.css'
 
-const LocationSelector = ({setWordOnSubmit, setLatitude, setLongitude}) => {
+const LocationSelector = ({setWordOnSubmit, setLatitude, setLongitude, setTest}) => {
 
     return (
-        <div>
-            <p>Location Selector</p>
+        <div className='body'>
             <Formik 
                 initialValues={{
                     latitude: '',
@@ -22,15 +22,28 @@ const LocationSelector = ({setWordOnSubmit, setLatitude, setLongitude}) => {
                         setWordOnSubmit(res.data);
                         setLatitude(values.latitude);
                         setLongitude(values.longitude);
+                        let i = 0; 
+                        let objArr = [];
+                        while (i < res.data.length) {
+                            let newObj = {
+                                name: res.data[i],
+                                place: res.data[i + 1],
+                                dist: res.data[i + 2]
+                            }
+                            i += 4;
+                            objArr.push(newObj);
+                        }
+                        console.log(objArr);
+                        setTest(objArr);
                     })
                       .catch(err => console.log(err));
                 }}
             >
-                <Form>
-                    <Field id="latitude" name="latitude" placeholder="Latitude" />
-                    <Field id="longitude" name="longitude" placeholder="Longitude" />
-                    <Field id="radius" name="radius" placeholder="Search Radius" />
-                    <button type="submit">Submit</button>
+                <Form className='fullForm'>
+                    <Field id="latitude" name="latitude" placeholder="Latitude (-180, 180)" className="fieldBox" />
+                    <Field id="longitude" name="longitude" placeholder="Longitude (-180, 180)" className="fieldBox" />
+                    <Field id="radius" name="radius" placeholder="Search Radius" className="fieldBox" />
+                    <button type="submit" className='submitButton'>Submit</button>
                 </Form>
             </Formik>
         </div>
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   const [currLat, setLat] = useState('');
   const [currLon, setLon] = useState('');
   const [zoom, setZoom] = useState(10);
+  const [test, setTest] = useState([]);
 
   const zoomIn = () => {
     if (zoom === 20) return;
@@ -21,11 +22,34 @@ const App = () => {
 
   return (
     <div className="App">
-      <LocationSelector setWordOnSubmit={setLocations} setLatitude={setLat} setLongitude={setLon} />
-      <div>
-        {locations.map((value, index) => <li key={index}>{value}</li>)}
-        <div>
-          <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${currLat == '' ? '33.7490' : currLat},${currLon == '' ? '-84.3880' : currLon}&zoom=${zoom}&size=400x400${locations.length === 0 ? `` : `&markers=color:blue%7C${currLat == '' ? '33.7490' : currLat},${currLon == '' ? '-84.3880' : currLon}`}&key=AIzaSyBjZXpz2X3e0KhnzBK8Av7IDnbwfm0ZowU`} alt="" />
+      <div className='selectorTable'>
+        <div className='selectorBody'>
+          <LocationSelector setWordOnSubmit={setLocations} setLatitude={setLat} setLongitude={setLon} setTest={setTest} />
+        </div>
+        <div className='tableBody'>
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Type of Place</th>
+              <th>Distance</th>
+            </tr>
+            {
+              test.map(location => {
+                return (
+                  <tr>
+                    <td>{location.name}</td>
+                    <td>{location.place}</td>
+                    <td>{location.dist}</td>
+                  </tr>
+                );
+              })
+            }
+          </table>
+        </div>
+      </div>
+      <div className='map'>
+        <img src={`https://maps.googleapis.com/maps/api/staticmap?center=${currLat == '' ? '33.7490' : currLat},${currLon == '' ? '-84.3880' : currLon}&zoom=${zoom}&size=500x500${locations.length === 0 ? `` : `&markers=color:blue%7C${currLat == '' ? '33.7490' : currLat},${currLon == '' ? '-84.3880' : currLon}`}&key=AIzaSyBjZXpz2X3e0KhnzBK8Av7IDnbwfm0ZowU`} alt="" />
+        <div className='buttons'>
           <button onClick={zoomIn}>Zoom In</button>
           <button onClick={zoomOut}>Zoom Out</button>
         </div>
